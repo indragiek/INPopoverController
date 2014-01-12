62e99f274555ee51e0ec37ccef54c6d700a7402f
Indragie Karunaratne <i@indragie.com>
2014-01-11 19:06:24 -0700

    Add alpha setting support to colour wells in sample app

---
+++ PopoverSampleApp/PopoverSampleApp/AlphaColorWell.h
@@ -0,0 +1,13 @@
+//
+//  AlphaColorWell.h
+//  PopoverSampleApp
+//
+//  Created by Indragie Karunaratne on 1/11/2014.
+//  Copyright (c) 2014 PCWiz Computer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AlphaColorWell : NSColorWell
+
+@end
