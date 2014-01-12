577326eb7f1e908d328136c79da35c472485dfe6
Indragie Karunaratne <i@indragie.com>
2014-01-11 19:24:19 -0700

    Move license to LICENSE file, update copyright

--- PopoverSampleApp/PopoverSampleApp/main.m
+++ PopoverSampleApp/PopoverSampleApp/main.m
@@ -3,7 +3,7 @@
 //  PopoverSampleApp
 //
 //  Created by Indragie Karunaratne on 11-03-02.
-//  Copyright 2011 PCWiz Computer. All rights reserved.
+//  Copyright 2011-2014 PCWiz Computer. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
